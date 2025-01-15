be6c7693285f38a9a4c2203a318d8e95610fa281
Ken Zimny <kenneth.zimny@gmail.com>
2025-01-14 18:47:34 -0600

    Fetch and display DB data

--- src/app/page.js
+++ src/app/page.js
@@ -1,19 +1,38 @@
 "use client";
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [coffeeData, setCoffeeData] = useState([]);
+
   useEffect(() => {
     fetch("/api/get-items")
-      .then(() => console.log("Fetched /api/get-items"))
-      .catch(console.error);
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setCoffeeData(data);
+      })
+      .catch((err) => {
+        console.error("Error fetching data:", err);
+      });
   }, []);
 
   return (
     <>
       <div>
         <h1>Coffee Logger</h1>
-        <p>How much coffee do I drink????</p>
+        <p>Number of Pots Brewed: {coffeeData.length}</p>
+        <hr />
+        <p>Data:</p>
+        <ul>
+          {coffeeData.map((item) => {
+            return <li key={item.EventID}>{item.Timestamp}</li>;
+          })}
+        </ul>
       </div>
     </>
   );
