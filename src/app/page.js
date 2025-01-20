c1934f1c9605c9983cdb112bd32833eefd223d0b
Ken Zimny <kenneth.zimny@gmail.com>
2025-01-20 17:43:38 -0600

    Cleanup

--- src/app/page.js
+++ src/app/page.js
@@ -4,59 +4,59 @@ import { useEffect, useState } from "react";
 import mockdata from "@/app/mockdata.json";
 
 export default function Home() {
-  const [coffeeData, setCoffeeData] = useState([]);
+  const [rawData, setRawData] = useState([]);
 
   // Load Data
-  useEffect((isLiveData = false) => {
-    if (!isLiveData) setCoffeeData(mockdata)
+  useEffect((isDBData = false) => {
+    if (!isDBData) setRawData(mockdata)
     else {
       fetch("/api/get-items")
         .then((response) => {
             if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`)
             return response.json();
         })
-        .then((response) => setCoffeeData(response))
+        .then((response) => setRawData(response))
         .catch((err) => console.error("Error fetching data:", err));
     }
   }, []);
 
-  const parseData = (data) => {
+  const formatData = (rawData) => {
     const formattedDate = {
       year: "",
       month: "",
       day: "",
       hour: ""
     }
-
     
-    let hourArr = []
+    let formattedArray = []
     const datePattern = /^(\d{4})-(\d{2})-(\d{2})T(\d{2}).*/;
-    data.map((item)=>{
+    rawData.map((item)=>{
       const dataMatched = item.Timestamp.match(datePattern)
-      console.log("dataMatched)",dataMatched)
       const dataObject = Object.create(formattedDate)
-      dataObject.year = dataMatched[1]
+
+      dataObject.year  = dataMatched[1]
       dataObject.month = dataMatched[2]
-      dataObject.day = dataMatched[3]
-      dataObject.hour = dataMatched[4]
-      hourArr.push(dataObject)
+      dataObject.day   = dataMatched[3]
+      dataObject.hour  = dataMatched[4]
+      
+      formattedArray.push(dataObject)
     })
-    return hourArr
+    return formattedArray
   }
 
   useEffect(()=>{
-    console.log(parseData(coffeeData))
-  },[coffeeData])
+    console.log(formatData(rawData))
+  },[rawData])
 
   return (
     <>
       <div>
         <h1>Coffee Logger</h1>
-        <p>Number of Pots Brewed: {coffeeData.length}</p>
+        <p>Number of Pots Brewed: {rawData.length}</p>
         <hr />
         <p>Data:</p>
         <ul>
-          {coffeeData.map((item) => {return <li key={item.EventID}>{item.Timestamp}</li>})}
+          {rawData.map((item) => {return <li key={item.EventID}>{item.Timestamp}</li>})}
         </ul>
       </div>
     </>
