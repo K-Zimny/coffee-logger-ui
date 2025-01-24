6adcb9e9c39b692c3607ff418da2f39f33c38c2c
Ken Zimny <kenneth.zimny@gmail.com>
2025-01-24 09:16:20 -0600

    Render Tally Work, formatting.

--- src/app/page.js
+++ src/app/page.js
@@ -4,11 +4,12 @@ import { useEffect, useState } from "react";
 import mockdata from "@/app/mockdata.json";
 
 export default function Home() {
-  const [rawData, setRawData] = useState([])
-  const [cleanData, setCleanData] = useState([])
+  const [talliedData ,setTalliedData] = useState(false)
+  const [cleanData   ,setCleanData]   = useState([])
+  const [rawData     ,setRawData]     = useState([])
 
   // Load Data
-  useEffect((isDBData = false) => {
+  useEffect((isDBData = true) => {
     if (!isDBData) setRawData(mockdata)
     else {
       fetch("/api/get-items")
@@ -22,22 +23,22 @@ export default function Home() {
   }, []);
 
   const formatData = (rawData) => {
-    let formattedArray = []
-    const datePattern = /^(\d{4})-(\d{2})-(\d{2})T(\d{2}).*/;
+    const formattedArray = []
+    const datePattern    = /^(\d{4})-(\d{2})-(\d{2})T(\d{2}).*/;
 
     class BrewEvent {
       constructor(id, year, month, day, hour) {
-        this.id = id
-        this.year = parseInt(year)
-        this.month = parseInt(month)
-        this.day = parseInt(day)
-        this.hour = parseInt(hour)
+        this.id    = id
+        this.year  = year
+        this.month = month
+        this.day   = day
+        this.hour  = hour
       }
     }
 
     rawData.map((item)=>{
       const dataMatched = item.Timestamp.match(datePattern)
-      const dataObject = new BrewEvent(item.EventID, dataMatched[1], dataMatched[2], dataMatched[3], dataMatched[4])
+      const dataObject  = new BrewEvent(item.EventID, dataMatched[1], dataMatched[2], dataMatched[3], dataMatched[4])
       
       formattedArray.push(dataObject)
     })
@@ -45,30 +46,51 @@ export default function Home() {
   }
 
   const tallyData = (cleanData) => {
-    let tallyObj = {}
-    cleanData.map(({year, month, day, hour})=>{
-      if (tallyObj[year]) tallyObj[parseInt(year)]++
-      else tallyObj[year] = 1
+    const tallyObj = {
+      "monthGroup": {},
+      "yearGroup":  {},
+      "hourGroup":  {},
+      "dayGroup":   {}
+    }
+
+    cleanData.map(({month, year, hour, day})=>{
+      if(tallyObj["monthGroup"][month])  {tallyObj["monthGroup"][month]++}  else{tallyObj["monthGroup"][month] = 1}
+      if(tallyObj["yearGroup"]  [year])  {tallyObj["yearGroup"]  [year]++}  else{tallyObj["yearGroup"]  [year] = 1}
+      if(tallyObj["hourGroup"]  [hour])  {tallyObj["hourGroup"]  [hour]++}  else{tallyObj["hourGroup"]  [hour] = 1}
+      if(tallyObj["dayGroup"]    [day])  {tallyObj["dayGroup"]    [day]++}  else{tallyObj["dayGroup"]    [day] = 1}
     })
+
   return tallyObj
+  }
+
+  const renderTallyData = (talliedData) => {
+    if(!talliedData) return
+
+    let renderTallyArray = []
+    for(let i = 0; i < Object.keys(talliedData.yearGroup).length; i++){
+      renderTallyArray.push([Object.keys(talliedData.yearGroup)[i],Object.values(talliedData.yearGroup)[i]])
+    }
   }
 
   // Effects ================================================================================================================================================
   useEffect(()=>{
-    console.log(formatData(rawData))
     setCleanData(formatData(rawData))
   },[rawData])
 
   useEffect(()=>{
-    console.log(tallyData(cleanData))
+    setTalliedData(tallyData(cleanData))
   },[cleanData])
 
+  useEffect(()=>{
+    renderTallyData(talliedData)
+  },[talliedData])
 
   return (
     <>
       <div>
         <h1>Coffee Logger</h1>
         <p>Number of Pots Brewed: {rawData.length}</p>
+        {talliedData.yearGroup && <p>{Object.keys(talliedData.yearGroup)[0]}:{Object.values(talliedData.yearGroup)[0]}</p>}
         <hr />
         <p>Data:</p>
         <ul>
