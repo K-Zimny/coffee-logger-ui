c64e7e5a840d3ae69b3eb7ecdff665f669720f3f
Ken Zimny <kenneth.zimny@gmail.com>
2025-02-02 15:14:22 -0600

    Updates Route to use API Endpoint.

--- src/app/api/get-items/route.js
+++ src/app/api/get-items/route.js
@@ -1,34 +1,23 @@
 import { NextResponse } from "next/server";
-import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
-import { ScanCommand } from "@aws-sdk/lib-dynamodb";
 
-function getCredentials() {
-  if (process.env.ACCESS_KEY_ID && process.env.SECRET_ACCESS_KEY) {
-    return {
-      accessKeyId: process.env.ACCESS_KEY_ID,
-      secretAccessKey: process.env.SECRET_ACCESS_KEY,
-    };
-  }
-  return undefined;
-}
-
-const client = new DynamoDBClient({
-  region: process.env.REGION || "",
-  credentials: getCredentials(),
-});
+const API_KEY = process.env.API_KEY;
+const API_ENDPOINT = process.env.API_ENDPOINT;
 
 export async function GET() {
   try {
-    const params = {
-      TableName: process.env.TABLE_NAME,
-    };
+    const response = await fetch(API_ENDPOINT, {
+      headers: {
+        "x-api-key": API_KEY,
+      },
+    });
 
-    const data = await client.send(new ScanCommand(params));
+    const data = await response.json();
 
-    return NextResponse.json(data.Items ?? []);
-  } catch (err) {
+    return NextResponse.json(JSON.parse(data.body));
+  } 
+  catch (err) {
     console.error("Scan error:", err);
-
+    
     return NextResponse.json(
       { error: "Error fetching items" },
       { status: 500 }
